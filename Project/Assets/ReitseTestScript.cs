afc1e392f678fa27a34ab3d91d5ba7910e331188
Mark de Heij <markdeheij@hotmail.com>
2015-03-12 16:31:18 +0100

    Trying to implement multiple spine animations
    Still working on it though.

--- Project/Assets/ReitseTestScript.cs
+++ Project/Assets/ReitseTestScript.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class ReitseTestScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SpineAnimation]
+    public string idleAnimation;
+
+    [SpineAnimation]
+    public string runningAnimation;
+
+
+    [Range(0f, 10f)]
+    public float moveSpeed;
+
+    private SkeletonAnimation reitseSkeleton;
+
+    void Awake()
+    {
+        reitseSkeleton = GetComponent<SkeletonAnimation>();
+    }
+
+    void Update()
+    {
+        if((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.D)))
+        {
+            //move right
+            reitseSkeleton.AnimationName = runningAnimation;
+            reitseSkeleton.skeleton.FlipX = false;
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        else if ((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.A)))
+        {
+            //move left
+            reitseSkeleton.AnimationName = runningAnimation;
+            reitseSkeleton.skeleton.FlipX = true;
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            reitseSkeleton.AnimationName = idleAnimation;
+        }
+
+    }
+
+
 }
