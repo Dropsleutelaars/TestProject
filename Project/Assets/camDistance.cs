0ba1f17e9a4ca4ff08991b9cbb21d97fb83ee1d5
Mark de Heij <markdeheij@hotmail.com>
2015-04-20 16:12:26 +0200

    Last Commit Of The Day
    Updated some assets

--- Project/Assets/camDistance.cs
+++ Project/Assets/camDistance.cs
@@ -4,25 +4,43 @@ using System.Collections;
 public class camDistance : MonoBehaviour 
 {
     public float hSliderValue;
+    public Player playerSettings;
+
+    public float playerJumpForce;
+
 
     void Awake()
     {
         hSliderValue = Camera.main.orthographicSize;
+        Screen.SetResolution(1024, 768, true);
     }
     
     
     void OnGUI()
     {
+        // Make a background box
+        GUI.Box(new Rect(00, 00, 250, 500), "Settings Menu");
+
+            GUI.Label(new Rect(10, 20, 250, 20), "Camera Settings:");
+
+            GUI.Label(new Rect(10, 40, 250, 20), "Distance: " + hSliderValue);
+            hSliderValue = GUI.HorizontalSlider(new Rect(10, 60, 230, 20), hSliderValue, 1.0F, 20.0F);
+
+            GUI.Label(new Rect(10, 80, 250, 20), "Player Settings:");
+
+            GUI.Label(new Rect(10, 100, 250, 20), "JumpPower: " + playerSettings.jumpForce);
+            playerSettings.jumpForce = GUI.HorizontalSlider(new Rect(10, 120, 230, 20), playerSettings.jumpForce, 400.0F, 800.0F);
+
+            GUI.Label(new Rect(10, 140, 250, 20), "maxSpeed: " + playerSettings.maxSpeed);
+            playerSettings.maxSpeed = GUI.HorizontalSlider(new Rect(10, 160, 230, 20), playerSettings.maxSpeed, 1.0F, 20.0F);
         
-        GUI.Label(new Rect(10, 10, 250, 20), "Change the Camera Distance");
-        GUI.Label(new Rect(30, 30, 250, 20), "Current distance: " + hSliderValue);
-    
-        hSliderValue = GUI.HorizontalSlider(new Rect(50, 50, 250, 20), hSliderValue, 1.0F, 15.0F);
     }
 
     void Update()
     {
-        
         Camera.main.orthographicSize = hSliderValue;
+        playerJumpForce = playerSettings.jumpForce;
+        
+       // Debug.Log(playerSettings.jumpForce);
     }
 }
