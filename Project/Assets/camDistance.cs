11760e19433963d85dbcb946bd14a44a81d21125
Mark de Heij <markdeheij@hotmail.com>
2015-04-20 11:16:22 +0200

    Updated the Folderstructure and MainCamera
    - Deleted some unnecessary files
    - The cameraOrthographicSize is now editable from the slider.

---
+++ Project/Assets/camDistance.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camDistance : MonoBehaviour 
+{
+    public float hSliderValue;
+
+    void Awake()
+    {
+        hSliderValue = Camera.main.orthographicSize;
+    }
+    
+    
+    void OnGUI()
+    {
+        
+        GUI.Label(new Rect(10, 10, 250, 20), "Change the Camera Distance");
+        GUI.Label(new Rect(30, 30, 250, 20), "Current distance: " + hSliderValue);
+    
+        hSliderValue = GUI.HorizontalSlider(new Rect(50, 50, 250, 20), hSliderValue, 1.0F, 15.0F);
+    }
+
+    void Update()
+    {
+        
+        Camera.main.orthographicSize = hSliderValue;
+    }
+}
