3264bbcc8abb077433011b780273af21acf31332
Mark de Heij <markdeheij@hotmail.com>
2015-05-18 15:45:43 +0200

    Added a new scene and assets
    This is what im going to use to merge Davidovs and my project.

---
+++ Project/Assets/____MERGED/Scripts/GameManagerScript.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MenuTypes : byte
+{
+    MainMenu = 0,
+    OptionsMenu = 1,
+    PauseMenu = 2,
+    GameOverMenu = 3,
+}
+
+public class GameManagerScript : MonoBehaviour
+{
+    // publics +
+    public AudioClip ClickSound;
+    public MenuTypes ActiveMenu { get; set; }
+    public bool IsMenuActive { get; set; }
+    // privates - 
+    private AudioSource m_SoundSource;
+    private Settings m_Settings = new Settings();
+    private readonly GUI.WindowFunction[] MenuFunctions = null;
+    private readonly string[] MenuNames = new string[]
+        {
+        "MainMenu",       //0
+        "OptionsMenu",  //1
+        "PauseMenu",      //2
+        "GameOverMenu",       //3
+        };
+
+    // waarom is een constructor ook geel? Vanwege de namespace aanduiding?
+
+    public GameManagerScript()
+    {
+        MenuFunctions = new GUI.WindowFunction[]
+        {
+        MainMenu,       //0
+        OptionsMenu,  //1
+        PauseMenu,      //2
+        GameOverMenu,       //3
+        };
+    }
+
+    void Awake()
+    {
+        ActiveMenu = MenuTypes.MainMenu;
+        IsMenuActive = false;
+        Application.runInBackground = true;
+
+        DontDestroyOnLoad(gameObject);
+
+        m_SoundSource = Camera.main.transform.FindChild("Sound").GetComponent<AudioSource>();
+
+        m_Settings.Load(Camera.main.transform.FindChild("Music").GetComponent<AudioSource>(), m_SoundSource);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        const int Width = 400;
+        const int Height = 300;
+        // dit zorgt er voor dat het menu afsluit wanneer men op start spelen drukt.
+        if (IsMenuActive)
+        {
+            Rect windowRect = new Rect(
+                (Screen.width - Width) / 2,
+                (Screen.height - Height) / 2,
+                Width, Height);
+            GUILayout.Window(0, windowRect, MenuFunctions[(byte)ActiveMenu], MenuNames[(byte)ActiveMenu]);
+        }
+    }
+
+    private void MainMenu(int id)
+    {
+        if (GUILayout.Button("Start het spel"))
+        {
+            m_SoundSource.PlayOneShot(ClickSound);
+            IsMenuActive = false;
+        }
+        if (GUILayout.Button("Opties"))
+        {
+            m_SoundSource.PlayOneShot(ClickSound);
+            ActiveMenu = MenuTypes.OptionsMenu;
+        }
+        if (GUILayout.Button("Stop het spel"))
+        {
+            m_SoundSource.PlayOneShot(ClickSound);
+            Application.Quit();
+        }
+    }
+
+    private void OptionsMenu(int id)
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Muziek volume: ", GUILayout.Width(90));
+        m_Settings.MusicVolume = GUILayout.HorizontalSlider(m_Settings.MusicVolume, 0.0f, 1.0f);
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Geluid volume: ", GUILayout.Width(90));
+        m_Settings.SoundVolume = GUILayout.HorizontalSlider(m_Settings.SoundVolume, 0.0f, 1.0f);
+        GUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Reset High Score"))
+        {
+            m_SoundSource.PlayOneShot(ClickSound);
+            m_Settings.HighScore = 0;
+        }
+
+        if (GUILayout.Button("Back"))
+        {
+            m_SoundSource.PlayOneShot(ClickSound);
+            m_Settings.Save();
+            ActiveMenu = MenuTypes.MainMenu;//m_SourceMenu;
+        }
+    }
+
+    private void PauseMenu(int id)
+    {
+    }
+    private void GameOverMenu(int id)
+    {
+    }
+}
+
+
---
+++ Project/Assets/____MERGED/Scripts/PlayerMovement.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // speler beweging
+    public float Speed = 10.0f;
+    public float JumpSpeed = 10.0f;
+    // systeem
+    private Rigidbody2D rigidbody2D;
+    public LayerMask GroundLayers;
+    public LayerMask WallLayers;
+    // fields
+    private Animator m_Animator;
+    private Transform m_GroundCheck;
+    private Transform m_WallCheck;
+
+
+
+    private bool isWallSliding;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+        m_GroundCheck = transform.FindChild("GroundCheck");
+        m_WallCheck = transform.FindChild("WallCheck");
+    }
+
+   
+
+    void wallJump()
+    {
+        rigidbody2D.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+        isWallSliding = false;
+    }
+
+    void Update()
+    {
+        isWallSliding = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Debug.Log(rigidbody2D.velocity);
+        
+        isWallSliding = Physics2D.OverlapPoint(m_WallCheck.position, WallLayers);
+        bool isGrounded = Physics2D.OverlapPoint(m_GroundCheck.position, GroundLayers);
+        if (Input.GetButton("Jump"))
+        {
+            if (isGrounded)
+            {
+                rigidbody2D.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+                isGrounded = false;
+            }
+        }
+
+        if (isWallSliding)
+        {
+           // Debug.Log("Is wall sliding");
+            rigidbody2D.velocity = new Vector2(0f, -1f);
+
+            if (
+                (Input.GetAxis("Horizontal") > 0) && (this.transform.localScale == new Vector3(-1.0f, 1.0f, 1.0f)) 
+                ||
+                (Input.GetAxis("Horizontal") < 0) && (this.transform.localScale == new Vector3(1.0f, 1.0f, 1.0f))
+                )
+            {
+                wallJump();
+            }
+
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                rigidbody2D.velocity = new Vector2(0f, -7f);
+            }
+        }
+      
+
+//         // WALLJUMP
+//         if (Input.GetButton("Jump"))
+//         {
+//             if (isWallSliding)
+//             {
+//                 rigidbody2D.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+//                 isWallSliding = false;
+//             }
+//         }
+
+
+        m_Animator.SetBool("IsGrounded", isGrounded);
+        float hSpeed = (Input.GetAxis("Horizontal"));
+
+
+        Debug.Log(hSpeed);
+
+        m_Animator.SetFloat("Speed", Mathf.Abs(hSpeed));
+
+        if (hSpeed > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (hSpeed < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        this.rigidbody2D.velocity = new Vector2(hSpeed * Speed, this.rigidbody2D.velocity.y);
+    }
+
+    
+}
