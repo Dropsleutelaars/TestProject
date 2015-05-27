d25782bcdadd011bdfd6abadc13854e9ec111337
David Vorsteveld <davidvorsteveld@gmail.com>
2015-05-27 11:45:46 +0200

    Added the ability to change scenes in the main menu

---
+++ Project/Assets/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {	
+	
+	// Update is called once per frame
+	public void ChangeToScene (string sceneToChangeTo) 
+    {
+        Application.LoadLevel(sceneToChangeTo);
+
+	}
+}
